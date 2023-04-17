6c900cf209402a27450a03507fa8b282248ebac4
furkangece <62790921+furkangece@users.noreply.github.com>
2023-04-17 14:59:33 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private void InitializeComponent()
+        {
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.disconnectBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.connectBtn.Location = new System.Drawing.Point(135, 52);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(114, 64);
+            this.connectBtn.TabIndex = 0;
+            this.connectBtn.Text = "Connect";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            this.connectBtn.Click += new System.EventHandler(this.connectBtn_Click_1);
+            // 
+            // disconnectBtn
+            // 
+            this.disconnectBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.disconnectBtn.Location = new System.Drawing.Point(291, 52);
+            this.disconnectBtn.Name = "disconnectBtn";
+            this.disconnectBtn.Size = new System.Drawing.Size(121, 64);
+            this.disconnectBtn.TabIndex = 1;
+            this.disconnectBtn.Text = "Disconnect";
+            this.disconnectBtn.UseVisualStyleBackColor = true;
+            this.disconnectBtn.Click += new System.EventHandler(this.disconnectBtn_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(247, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(176, 202);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.connectBtn);
+            this.Controls.Add(this.disconnectBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.Button disconnectBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using EasyModbus;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        Thread thread;
+        public void plc_connect()
+        {
+            modbusClient = new ModbusClient("10.0.0.41", 502);
+            modbusClient.Connect();
+            thread = new Thread(new ThreadStart(updscreen));
+
+            thread.Start();
+        }
+
+        ModbusClient modbusClient;
+        private void connectBtn_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                plc_connect();
+                label1.Text = "Bağlantı Sağlandı.";
+            }
+            catch
+            {
+                modbusClient.Disconnect();
+                label1.Text = "Bağlantı Sağlanamadı.";
+                textBox1.Text = "";
+            }
+
+        }
+        private void disconnectBtn_Click_1(object sender, EventArgs e)
+        {
+            thread.Suspend();
+            modbusClient.Disconnect();
+            label1.Text = "Bağlantı Kesildi.";
+            textBox1.Text = "";
+
+
+        }
+        int[] readHoldingRegisters;
+
+        public void updscreen()
+        {
+            while (true)
+            {
+                readHoldingRegisters = modbusClient.ReadHoldingRegisters(10, 1);
+                double dreadHoldingRegisters = Convert.ToDouble(readHoldingRegisters[0]);
+                textBox1.Text = (dreadHoldingRegisters / 1000).ToString();
+
+            }
+        }
+        }
+    }
